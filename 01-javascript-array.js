e344f04979b275273117902a7e04cb7b7e5f3ac0
iGrizzz <muda3332@gmail.com>
2021-10-11 21:48:32 +0700

    Tugas Done

--- 01-javascript-array.js
+++ 01-javascript-array.js
@@ -1,16 +1,13 @@
+
+
 const arr = [1, 2, 3];
 const arr2 = [8, 1, 5, 7];
-function reverse(param1) {
-    start = 0;
-    end = param1.length-1;
-while (start < end){
-    let temp = param1[start];
-    param1[start] = param1[end];
-    param1[end] = temp;
-    start = start + 1;
-    end = end - 1;
-}
-return param1;
+function reverse(param) { 
+    let data = [];
+    for(let i = 0;i < param.length; i++){
+        data[i] = param[i];
+    }
+    return data.reverse();
 }
 
 const newArr = reverse(arr);
